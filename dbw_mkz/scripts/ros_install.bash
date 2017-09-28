5cea444dfbab800f2099620ec18c39bcc22d603f
Kevin Hallenbeck <khallenbeck@dataspeedinc.com>
2017-09-28 09:27:38 -0400

    Updated install keys

--- dbw_mkz/scripts/ros_install.bash
+++ dbw_mkz/scripts/ros_install.bash
@@ -41,7 +41,7 @@ fi
 # Install ROS
 echo "Installing ROS $ROS_DISTRO..."
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
+sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
 sudo apt-get install -y ros-$ROS_DISTRO-desktop
 sudo rosdep init
