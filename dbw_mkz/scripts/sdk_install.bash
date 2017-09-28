8bcf1eb7dcacf10614a152cdeaa28ee133924d65
Kevin Hallenbeck <khallenbeck@dataspeedinc.com>
2017-09-28 09:27:38 -0400

    Updated install keys

--- dbw_mkz/scripts/sdk_install.bash
+++ dbw_mkz/scripts/sdk_install.bash
@@ -15,7 +15,7 @@ fi
 # Setup apt-get
 echo "Adding Dataspeed server to apt..."
 sudo sh -c 'echo "deb [ arch=amd64 ] http://packages.dataspeedinc.com/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-dataspeed-public.list'
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FF6D3CDA
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 66F84AE1EB71A8AC108087DCAF677210FF6D3CDA
 sudo apt-get update
 
 # Setup rosdep
